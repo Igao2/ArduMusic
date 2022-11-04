3fe833878937a98e32e89a4d446095e8b2573008
Richard <80765748+Richardcxz@users.noreply.github.com>
2022-11-04 17:21:39 -0300

    Add files via upload

---
+++ cover/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { dirname } = require("path");
+const app = express();
+const path = require('path');
+app.use(express.static(__dirname+'inst.png'))
+let request;
+const { SerialPort } = require('serialport');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
+//const arduino = new SerialPort({path:'/dev/ATMACY0',baudRate: 9600})
+app.get("/",function(req,res)
+{
+    res.sendFile(__dirname+"/index.html");
+});
+app.post('/',(req,res) =>
+{
+    request = req.body.note1;
+    console.log(request);
+});
+console.log("oi");
+app.listen(8080,function(){console.log("Server rodando")});
